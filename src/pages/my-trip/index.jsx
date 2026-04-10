b3c449635c0999bdb94dda06b3e94ecd4e571930
sanBastia <irsanoncode@gmail.com>
2026-04-10 15:52:40 +0700

    refactor: all state in mytrip and viewtrip are in zustand

--- src/pages/my-trip/index.jsx
+++ src/pages/my-trip/index.jsx
@@ -1,51 +1,48 @@
-import { db } from "@/service/firebaseConfig";
-import { collection, query, where, getDocs } from "firebase/firestore";
-import { useEffect, useState } from "react";
-import { useNavigation } from "react-router-dom";
+
+import { useEffect} from "react";
+import { useNavigate } from "react-router-dom";
 import UserTripCarditem from "./components/UserTripCarditem";
 import { SkeletonCard } from './components/SkeletonCard'
 import { useUserStore } from "@/store/useUserStore";
+import { useTripStore } from "@/store/useTripStore";
+import { useShallow } from "zustand/react/shallow";
+
+import EmptyTripState from "./components/EmptyTrip";
 
 function MyTrip() {
-  const navigate = useNavigation();
+  const navigate = useNavigate();
   const user = useUserStore((state)=>state.user)
+  const { userTrips, loading, fetchUserTrips } = useTripStore(useShallow((state)=>({
+        userTrips: state.userTrips,
+        loading: state.loading,
+        fetchUserTrips: state.fetchUserTrips
 
-  const [userTrips, setUserTrips] = useState([]);
+  })))
 
   useEffect(() => {
-    GetUserTrips();
-  }, []);
-
-  const GetUserTrips = async () => {
-    
-    if (!user) {
+      if (!user) {
       navigate("/");
       return;
     }
+    fetchUserTrips(user?.email)
+  }, [user,fetchUserTrips,navigate]);
 
-    const q = query(
-      collection(db, "trip"),
-      where("userEmail", "==", user?.email)
-    );
-    const querySnapshot = await getDocs(q);
-    setUserTrips([]);
-    querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      // console.log(doc.id, " => ", doc.data());
-      setUserTrips((prevVal) => [...prevVal, doc.data()]);
-    });
-  };
 
   return (
     <div className="sm:px-10 md:px-32 lg:px-56 xl-px-10 px-5 mt-10">
       <h2 className="font-bold text-3xl">My Trips</h2>
       <div className="grid grid-cols-2 mt-10 md:grid-cols-3 gap-5">
-        {userTrips && userTrips?.length > 0
-          ? userTrips.map((trip, index) => (
+        
+        {loading ?(
+          <SkeletonCard />
+        ): userTrips?.length > 0 ? (
+          userTrips.map((trip, index) => (
               <UserTripCarditem key={index} trip={trip} />
             ))
-          : <SkeletonCard />
-          }
+        ) : (
+          <EmptyTripState />
+        )}
+
       </div>
     </div>
   );
