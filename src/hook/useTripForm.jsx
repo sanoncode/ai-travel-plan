c929215daf9f38223cb062ad3defe2a1a013e030
sanBastia <irsanoncode@gmail.com>
2026-04-14 11:37:11 +0700

    refactor: useTripFrom also validate formData, and days

--- src/hook/useTripForm.jsx
+++ src/hook/useTripForm.jsx
@@ -1,23 +1,44 @@
 import { useCreateTripStore } from "@/store/useCreateTripStore";
 import { useShallow } from "zustand/shallow";
 
+import { toast } from "sonner";
+
 export const useTripForm = () => {
-  const { setField, setUi } = useCreateTripStore(
+  const { formData, setField, setUi } = useCreateTripStore(
     useShallow((state) => ({
+      formData: state.formData,
       setField: state.setField,
-      setUi: state.setUi,
+      setUi: state.setUi
     })),
   );
 
-  const setDays = (value) => {
-    //invalid for 0 days, and for above 7
-    const inValid = value < 1 || value > 7;
+  // ========================
+  // FORM VALIDATION (only for days)
+  // ========================
+  const setDays = (num) => {
+       
+    const isDaysInValid = num < 1 || num > 7
+      if(isDaysInValid) toast("Days must be between 1 and 7");
+      setUi('isDaysInvalid', isDaysInValid)
+      setField("days", num)
+  }
 
-    setUi("isDaysInvalid", inValid);
-    setField("days", value);
+  // ========================
+  // FORM VALIDATION (FINAL GATE)
+  // ========================
+  const validateForm = () => {
+    if (!formData.country || !formData.people || !formData.budget) {
+      return {
+        isValid: false,
+        message: "Please fill all fields",
+      };
+    }
+
+    return { isValid: true };
   };
 
   return {
-    setDays,
+    validateForm,
+    setDays
   };
 };
