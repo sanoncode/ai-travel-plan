283b49519e5aa132df179fb55f94acd15fae8c62
sanBastia <irsanoncode@gmail.com>
2026-04-14 11:38:16 +0700

    refactor: make this component get all state directly from zustand

--- src/pages/create-trip/components/AILoadingDialog.jsx
+++ src/pages/create-trip/components/AILoadingDialog.jsx
@@ -1,19 +1,33 @@
 /* eslint-disable react/prop-types */
-"use client"
+"use client";
 
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogTitle
+  DialogTitle,
 } from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button"
-import { Loader2, CheckCircle2, XCircle} from "lucide-react"
-import { Link } from "react-router-dom"
+import { Button } from "@/components/ui/button";
+import { Loader2, CheckCircle2, XCircle } from "lucide-react";
+import { Link } from "react-router-dom";
 import { useCreateTripStore } from "@/store/useCreateTripStore";
+import { useShallow } from "zustand/shallow";
 
+// ========================
+// ERROR MESSAGE MAPPING
+// ========================
+const ERROR_MESSAGE = {
+  AI_ERROR: "Failed to generate trip. Please try again.",
+  FIREBASE_ERROR: "Failed to save your trip. Please retry.",
+  INVALID_JSON: "Unexpected response from AI. Please retry.",
+  DEFAULT:
+    "This model is currently experiencing high demand. Please try again later.",
+};
 
+// ================
+// STEPS
+// ==========
 const generationSteps = [
   "Analyzing request and planning response",
   "Gathering relevant information",
@@ -21,23 +35,28 @@ const generationSteps = [
   "Generating detailed content",
   "Refining and polishing output",
   "Finalizing and preparing delivery",
-]
+];
 
 export function AILoadingDialog({
-  open,
-  status = "loading", // "loading" | "success" | "error" | "offline"
-  errorMessage,
   onRetry,
-  viewTripId,
 }) {
   const [currentStep, setCurrentStep] = useState(0);
 
-  const  reset = useCreateTripStore((state) => ( state.reset ));
+  const { generation, reset, ui, result } = useCreateTripStore(
+    useShallow((state) => ({
+      generation: state.generation,
+      ui: state.ui,
+      reset: state.reset,
+      result: state.result,
+    })),
+  );
 
-  const isLoading = status === "loading";
-  const isSuccess = status === "success";
-  const isError = status === "error";
-  const isOffline = status === "offline";
+  const errorType = generation.error;
+
+  const isLoading = generation.status === "loading";
+  const isSuccess = generation.status === "success";
+  const isError = generation.status === "error";
+  const isOffline = generation.status === "offline";
 
   useEffect(() => {
     if (!isLoading) return;
@@ -58,27 +77,19 @@ export function AILoadingDialog({
   }, [isLoading]);
 
   return (
-    <Dialog open={open}>
+    <Dialog open={ui.openGenerateDialog}>
       <DialogContent className="sm:max-w-md [&>button]:hidden">
         <div className="flex flex-col items-center space-y-6 py-4">
-
           {/* ICON / IMAGE */}
           <div className="flex items-center justify-center w-full rounded-full">
             {isLoading && (
               <img src="/placeholder.jpg" className="h-[300px] w-full p-2" />
             )}
 
-            {isSuccess && (
-              <CheckCircle2 className="h-10 w-10 text-green-500" />
-            )}
+            {isSuccess && <CheckCircle2 className="h-10 w-10 text-green-500" />}
 
-            {isError && (
-              <XCircle className="h-10 w-10 text-red-500" />
-            )}
-            {isOffline && (
-               <XCircle className="h-10 w-10 text-red-500" />
-            )
-            }
+            {isError && <XCircle className="h-10 w-10 text-red-500" />}
+            {isOffline && <XCircle className="h-10 w-10 text-red-500" />}
           </div>
 
           {/* TITLE */}
@@ -93,8 +104,9 @@ export function AILoadingDialog({
             <DialogDescription className="text-sm text-muted-foreground">
               {isLoading && "Please wait while our AI creates your travel plan"}
               {isSuccess && "Your travel plan is ready to view"}
-              {isError && (errorMessage || "This model is currently experiencing high demand. Spikes in demand are usually temporary. Please try again later.")}
-              {isOffline && "Your Connection seems unstable, please check your internet and try again"}
+              {isError && ERROR_MESSAGE[errorType]}
+              {isOffline &&
+                "Your Connection seems unstable, please check your internet and try again"}
             </DialogDescription>
           </div>
 
@@ -126,7 +138,9 @@ export function AILoadingDialog({
                       <div className="h-5 w-5 rounded-full border" />
                     )}
 
-                    <span className={`text-sm ${index <= currentStep ? "" : "text-muted-foreground"}`}>
+                    <span
+                      className={`text-sm ${index <= currentStep ? "" : "text-muted-foreground"}`}
+                    >
                       {step}
                     </span>
                   </div>
@@ -143,36 +157,44 @@ export function AILoadingDialog({
           )} */}
 
           {isSuccess && (
-            <Link to={"/view-trip/" + viewTripId}>
+            <Link to={"/view-trip/" + result.tripId}>
               <Button>View the plan!</Button>
             </Link>
           )}
 
           {isError && (
             <div className="flex gap-2">
-               <Button variant="outline" onClick={() => {
-                reset()
-                onRetry()
-              }}>
+              <Button
+                variant="outline"
+                onClick={() => {
+                  reset();
+                  onRetry();
+                }}
+              >
                 Try Again
               </Button>
-              <Button variant="outline" onClick={() => {
-                reset()
-              }}>
+              <Button
+                variant="outline"
+                onClick={() => {
+                  reset();
+                }}
+              >
                 Close
               </Button>
             </div>
           )}
           {isOffline && (
             <div className="flex gap-2">
-               <Button variant="outline" onClick={() => {
-                onRetry()
-              }}>
+              <Button
+                variant="outline"
+                onClick={() => {
+                  onRetry();
+                }}
+              >
                 Try Again
               </Button>
             </div>
           )}
-
         </div>
       </DialogContent>
     </Dialog>
