5386e1db6104e45dbf44c46fb783b172dad110c3
sanBastia <irsanoncode@gmail.com>
2026-04-21 15:41:44 +0700

    fix: change limit day to today

--- server/lib/limitTrip.js
+++ server/lib/limitTrip.js
@@ -4,15 +4,11 @@ export const checkLimitTrip = async (userId) => {
     const todayStart = new Date()
     todayStart.setHours(0, 0, 0, 0)
 
-    const yesterdayStart = new Date();
-    yesterdayStart.setDate(yesterdayStart.getDate() - 1);
-    yesterdayStart.setHours(0, 0, 0, 0);
-
     const { count, error } = await supabase
     .from('trips')
     .select('*', { count: "exact", head: true })
     .eq('user_id', userId )
-    .gte('created_at', yesterdayStart.toISOString());
+    .gte('created_at', todayStart.toISOString());
 
     if(error) throw error;
 
